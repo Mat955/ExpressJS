bf05abf69e31410a458958e7747300a926e5af36
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-06-17 20:43:40 +0200

    task 3 express

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('assets'));
+app.use('/store', function (req, res, next) {
+    console.log('I am an intermediary when requesting / store');
+    next();
+});
 
 app.get('/', function (req, res) {
-    res.sendFile('/index.html')
+    res.send('Hello world!');
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
-});
-var server = app.listen(3000, 'localhost', function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example application listen at address http://' + host + ':' + port);
+app.get('/store', function (req, res) {
+    res.send('This is shop');
 });
 
-
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry, we could not find what you want!');
+});
 
 
 
